3392e33aa74d8162f5ef408d094505cb8d41ec58
dominikdoric <dominikdoric277@gmail.com>
2023-09-08 13:16:16 +0200

    Linking Flutter project with Firebase

--- house_work_app/lib/main.dart
+++ house_work_app/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:house_work_app/firebase_options.dart';
 import 'package:house_work_app/presentation/screens/onboardingScreens/familyNameScreen/family_name_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
